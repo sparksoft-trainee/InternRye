467aa4cbbfe81659095fcf7acc20f3983a026ca8
Rye <mr.ryecorral@gmail.com>
2024-02-06 17:58:19 +0800

    little version testing

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Waazup MultiVerse!!!
         </a>
       </header>
     </div>
