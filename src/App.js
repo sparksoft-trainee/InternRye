1df8c455c0a02286143c065097f7464773367849
Rye <mr.ryecorral@gmail.com>
2024-02-07 15:29:10 +0800

    Amplify Auth Attempt 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Waazup MultiVerse!!!
+          Waazupppppp MultiVerse!!!
         </a>
       </header>
     </div>
